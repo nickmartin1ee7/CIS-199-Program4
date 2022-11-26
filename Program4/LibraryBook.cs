5ba6aee7d03a8a76a77a4ecae151462a6f028f65
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-11-26 15:49:09 -0500

    Committing old repo

---
+++ Program4/LibraryBook.cs
@@ -0,0 +1,119 @@
+﻿/**
+ *  Grading ID:  L8486
+ *  Class:       CIS 199-75
+ *  Program:     4
+ *  Due date:    12/04/19
+ *  Description: Library book object class.
+ **/
+
+using System;
+using static System.Console;
+using static System.Environment;
+
+public class LibraryBook
+{
+    const int DEFAULT_YEAR = 2019;  // Default copyright year
+    public string Title {   // Book's title
+        // Pre-Condition:  None
+        // Post-Condition: Returns a string
+        get;
+        // Pre-Condition:  None
+        // Post-Condition: Sets property to value as a string
+        set;
+    }
+    public string Author {  // Book's author
+        // Pre-Condition:  None
+        // Post-Condition: Returns a string
+        get;
+        // Pre-Condition:  None
+        // Post-Condition: Sets property to value as a string
+        set;
+    }
+    public string Publisher {   // Book's publisher
+        // Pre-Condition:  None
+        // Post-Condition: Returns a string
+        get;
+        // Pre-Condition:  None
+        // Post-Condition: Sets property to value as a string
+        set;
+        }
+    public string CallNumber {  // Book's call number
+        // Pre-Condition:  None
+        // Post-Condition: Returns a string
+        get;
+        // Pre-Condition:  None
+        // Post-Condition: Sets property to value as a string
+        set;
+        }
+    private int _copyrightYear; // Copyright year backing field
+    public int CopyrightYear {  // Book's copyright year
+        // Pre-Condition:  None
+        // Post-Condition: Returns an int
+        get {return _copyrightYear;}
+        // Pre-Condition:  0 < value < infinate
+        // Post-Condition: Sets property's backing field to value if positive, otherwise set to 2019
+        set {
+            if (value >= 0)
+                _copyrightYear = value;
+            else
+                _copyrightYear = DEFAULT_YEAR;
+            }
+        }
+    private bool CheckedOut {   // Book's checked out status. False = On shelf, True = Checked out. Private because will only be called via class method
+        // Pre-Condition:  None
+        // Post-Condition: Returns a boolean
+        get;
+        // Pre-Condition:  None
+        // Post-Condition: Sets property to value as a boolean
+        set;
+    }
+
+    // Constructor
+    // Pre-Condition:  4 Strings and 1 int passed to constructor
+    // Post-Condition: Constructs LibraryBook object with given arguments and by default not checked out
+    public LibraryBook(string title, string author, string publisher, int copyrightYear, string callNumber)
+    {
+        Title = title;
+        Author = author;
+        Publisher = publisher;
+        CopyrightYear = copyrightYear;
+        CallNumber = callNumber;
+        CheckedOut = false;    // Is book checked out flag. False by default.
+    }
+
+    // Methods
+    // Pre-Condition:  None
+    // Post-Condition: CheckedOut boolean will be true. Error message will be displayed if already true
+    public void CheckOut()
+    {
+        if (CheckedOut)
+            WriteLine("Error: already checked out");
+        else
+            CheckedOut = true;
+    }
+    
+    // Pre-Condition:  None
+    // Post-Condition: CheckedOut boolean will be false. Error message will be displayed if already false
+    public void ReturnToShelf()
+    {
+         if (CheckedOut)
+            CheckedOut = false;
+        else
+            WriteLine("Error: already on shelf");
+    }
+
+    // Pre-Condition:  None
+    // Post-Condition: Returns a boolean
+    public bool IsCheckedOut()
+    {
+        return CheckedOut;
+    }
+
+    // Pre-Condition:  None
+    // Post-Condition: Returns a string of all object's data in custom format
+    public override string ToString()
+    {
+        string result = $"Title: {Title}{NewLine}Author: {Author}{NewLine}Publisher: {Publisher}{NewLine}Copyright Year: {CopyrightYear}{NewLine}Call Number: {CallNumber}{NewLine}Is checked out? {CheckedOut}";
+        return result;
+    }
+}
---
+++ Program4/Program.cs
@@ -0,0 +1,55 @@
+﻿/**
+ *  Grading ID:  L8486
+ *  Class:       CIS 199-75
+ *  Program:     4
+ *  Due date:    12/04/19
+ *  Description: Library book management system. Demos the use of LibraryBook class.
+ **/
+
+using System;
+using static System.Console;
+using static System.Environment;
+
+class Program
+{
+    public static void Main()
+    {
+        const int NUM_OF_DEFAULT_BOOKS = 5; // Amount of default books
+        
+        // LibraryBook book = new LibraryBook("Title", "Author", "Publisher", 2000, "Call Number");
+        LibraryBook book1 = new LibraryBook("Tom's tales", "Tom", "Independant Authors", 2000, "111-111-111");
+        LibraryBook book2 = new LibraryBook("Tom's tails", "Tom", "Independant Authors", 2018, "111-222-111");
+        LibraryBook book3 = new LibraryBook("Top 100 airplane jokes", "Jefferson airplane", "Independant Authors", 2005, "111-333-111");
+        LibraryBook book4 = new LibraryBook("Secret diary", "Jake", "Independant Authors", -1, "111-111-222");
+        LibraryBook book5 = new LibraryBook("Tom is dumb", "Jake", "Independant Authors", -1, "111-111-333");
+        
+        // Populate book array
+        LibraryBook[] allBooks = new LibraryBook[NUM_OF_DEFAULT_BOOKS];
+        allBooks[0] = book1;
+        allBooks[1] = book2;
+        allBooks[2] = book3;
+        allBooks[3] = book4;
+        allBooks[4] = book5;
+
+        // DEMO
+        ListBooks(allBooks);
+        book2.CheckOut();
+        book4.CheckOut();
+        book1.Publisher = "Thomas";
+        book5.CallNumber = "123-456-789";
+        ListBooks(allBooks);
+        book2.ReturnToShelf();
+        book4.ReturnToShelf();
+        ListBooks(allBooks);
+        ReadKey();
+        // END DEMO
+    }
+
+    // Pre-Condition:  LibraryBook array must exist
+    // Post-Condition: Prints each LibraryBook object's ToString in the array
+    private static void ListBooks(LibraryBook[] allBooks)
+    {
+        for (int i = 0; i < allBooks.Length; ++i)
+            WriteLine($"{NewLine}Book #{i + 1} of {allBooks.Length}{NewLine}{allBooks[i].ToString()}");
+    }
+}
